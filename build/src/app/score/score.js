9e67558058c120ca3b306211e67c9f4864e79ed4
Oskar Konstantyner <oskar@konstanto.dk>
2014-08-06 14:06:01 +0200

    Initial setup

---
+++ build/src/app/score/score.js
@@ -0,0 +1,139 @@
+/**
+ * Each section of the site has its own module. It probably also has
+ * submodules, though this boilerplate is too simple to demonstrate it. Within
+ * `src/app/home`, however, could exist several additional folders representing
+ * additional modules that would then be listed as dependencies of this one.
+ * For example, a `note` section could have the submodules `note.create`,
+ * `note.delete`, `note.edit`, etc.
+ *
+ * Regardless, so long as dependencies are managed correctly, the build process
+ * will automatically take take of the rest.
+ *
+ * The dependencies block here is also where component dependencies should be
+ * specified, as shown below.
+ */
+angular.module( 'ngBoilerplate.score', [
+    'ui.router',
+    'plusOne'
+])
+
+/**
+ * Each section or module of the site can also have its own routes. AngularJS
+ * will handle ensuring they are all available at run-time, but splitting it
+ * this way makes each module more "self-contained".
+ */
+    .config(function config( $stateProvider ) {
+        $stateProvider.state( 'score', {
+            url: '/score',
+            views: {
+                "main": {
+                    controller: 'ScoreCtrl',
+                    templateUrl: 'score.tpl.html'
+                }
+            },
+            data:{ pageTitle: 'score' }
+        });
+    })
+
+/**
+ * And of course we define a controller for our route.
+ */
+    .controller( 'ScoreCtrl', function ScoreController( $scope, $http ) {
+        var me = $scope;
+
+        me.gameType = "normal";
+
+        me.pointArray = ["0", "15", "30", "40", "Ad"];
+
+        me.scoreData = {
+            players: [
+                {
+                    name: "Player 1",
+                    point: 0,
+                    party: 0,
+                    set: 0,
+                    tieBreak: 0
+                },
+                {
+                    name: "Player 2",
+                    point: 0,
+                    party: 0,
+                    set: 0,
+                    tieBreak: 0
+                }],
+            isTiebreak: 0
+        };
+
+        me.players = me.scoreData.players;
+
+
+        me.sendToServer = function(){
+            console.log('hej');
+
+            $http({
+                method: 'POST',
+                url: 'http://konstantyner.org/tennissporten/tennisScore.php',
+                data: "players=" + JSON.stringify(me.scoreData),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+
+            //$http.post("http://konstantyner.org/tennissporten/tennisScore.php",$.param({players: JSON.stringify(me.players)}), {'Content-Type': 'application/x-www-form-urlencoded'});
+        };
+
+
+        me.decreasePoint = function(player) {
+            if(player.point > 0){
+                player.point--;
+            }
+            me.sendToServer();
+        };
+
+        me.increasePoint = function(player, playerNumber) {
+            var otherPlayerId = (playerNumber === 0) ? 1 : 0;
+            var otherPlayer = me.players[otherPlayerId];
+            if(player.point < 5){
+                player.point++;
+
+                if(player.point > 3 && (player.point - otherPlayer.point > 1)){
+                    _.each(me.players, function(player){
+                        player.point = 0;
+                    });
+                    player.party++;
+                }
+            }
+            me.sendToServer();
+        };
+
+        me.decreaseParty = function(player) {
+            if(player.party > 0){
+                player.party--;
+            }
+            me.sendToServer();
+        };
+
+        me.increaseParty = function(player) {
+            player.party++;
+            _.each(me.players, function(player){
+                player.point = 0;
+            });
+            me.sendToServer();
+        };
+
+        me.decreaseSet = function(player) {
+            if(player.set > 0){
+                player.set--;
+            }
+            me.sendToServer();
+        };
+
+        me.increaseSet = function(player) {
+            player.set++;
+            _.each(me.players, function(player){
+                player.point = 0;
+                player.party = 0;
+            });
+            me.sendToServer();
+        };
+    })
+;
+
---
+++ src/app/app.js
@@ -0,0 +1,25 @@
+angular.module( 'ngBoilerplate', [
+  'templates-app',
+  'templates-common',
+  'ngBoilerplate.score',
+  'ngBoilerplate.about',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/score' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+    }
+  });
+})
+
+;
+
